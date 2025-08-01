bd559773a9d662fed6a81c011a5750ece9e3f854
gsonnier333 <gsonnier3@gmail.com>
2025-07-31 21:53:48 -0600

    New loop animation, multi-door toggling for buttons, stretching viewport with window size

--- src/scripts/pressure_plate.gd
+++ src/scripts/pressure_plate.gd
@@ -1,26 +1,25 @@
 extends Node2D
 class_name PressurePlate
 
-@export var activate_object: Node2D
+@export var activate_objects: Array[Node2D]
 
 @onready var activate_collision: CollisionShape2D = %ActivateCollision
 @onready var activate_sprite: Sprite2D = %ActivateSprite
 
 @onready var plate_base_sprite: Sprite2D = %PlateBaseSprite
-@onready var plate_base_collision: CollisionShape2D = %PlateBaseCollision
 
 
 func _ready() -> void:
-	if not activate_object:
+	if not activate_objects or activate_objects.is_empty():
 		push_error("Pressure plate not connected to an activateable object")
 
 func _on_pressure_plate_area_body_entered(body: Node2D) -> void:
 	if body is Player:
-		if activate_object.has_method("toggle"):
-			activate_object.toggle()
+		for obj in activate_objects:
+			if obj.has_method("toggle"):
+				obj.toggle()
 		activate_sprite.hide()
 		plate_base_sprite.show()
-
 
 func _on_pressure_plate_area_body_exited(body: Node2D) -> void:
 	if body is Player:
